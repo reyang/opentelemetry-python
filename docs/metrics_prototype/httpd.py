c1d91a18170194e37bab448a194027c19a067296
Reiley Yang <reyang@microsoft.com>
2021-02-10 22:29:57 -0800

    more httpd demo

--- docs/metrics_prototype/httpd.py
+++ docs/metrics_prototype/httpd.py
@@ -1,18 +1,11 @@
 import socketserver
-from http.server import SimpleHTTPRequestHandler
+import sys
+
 from opentelemetry import metrics
 from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.metrics.export import ConsoleMetricsExporter
 
-class MyHttpRequestHandler(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
-        self.end_headers()
-        self.wfile.write(bytes("Hello, world!", "ASCII"))
-
 if __name__ == '__main__':
-    import sys
     host = "127.0.0.1"
     port = 7777
     if len(sys.argv) >= 2:
@@ -20,7 +13,18 @@ if __name__ == '__main__':
     if len(sys.argv) >= 3:
         port = int(sys.argv[2])
 
-    server = socketserver.TCPServer((host, port), MyHttpRequestHandler)
     metrics.set_meter_provider(MeterProvider())
-    print("Server listening on http://{host}:{port}".format(host=host, port=port))
+    # Should the instrumention library pick the meter name, or we will provide the meter to the instrumentation library, or we want to support both?
+    meter = metrics.get_meter("opentelemetry.httpd.server_library")
+    # How do we describe which time series we care about vs. not.
+    # * How do we know which time series are available from the instrumentation library?
+    # * For time series that we don't need, do we drop the them at processor level (pay for the collection cost and then drop the data on the floor)?
+    # How do we describe which dimension we care about vs. not?
+    # * For dimensions that are relatively expensive to gather, is there a way to avoid the cost at collection time?
+    # If we need different time window (e.g. hourly report for temperature, but 5 seconds report for CPU utilization) what do we do?
+    metrics.get_meter_provider().start_pipeline(meter, ConsoleMetricsExporter(), 5)
+
+    from server_library import OTelHttpRequestHandler
+    server = socketserver.TCPServer((host, port), OTelHttpRequestHandler)
+    print("Starting httpd on http://{host}:{port}".format(host=host, port=port))
     server.serve_forever()
---
+++ docs/metrics_prototype/server_library.py
@@ -0,0 +1,88 @@
+import os
+import random
+import socket
+import time
+from http.server import SimpleHTTPRequestHandler
+from opentelemetry import metrics
+
+HOST_NAME = socket.gethostname()
+PROCESS_ID = os.getpid()
+
+meter = metrics.get_meter("opentelemetry.httpd.server_library")
+# https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/semantic_conventions/http-metrics.md#http-server
+request_latency_recorder = meter.create_valuerecorder(name="http.server.duration", description="measures the duration of the inbound HTTP request", unit="milliseconds", value_type=float)
+
+meter.register_valueobserver(
+    callback=lambda observer: observer.observe(random.uniform(32, 100), {
+        "host.name": HOST_NAME,
+    }),
+    name="room_temperature",
+    description="the temperature of the server room",
+    unit="Fahrenheit",
+    value_type=float,
+)
+
+meter.register_valueobserver(
+    callback=lambda observer: observer.observe(int(random.uniform(0, 100)), {
+        "host.name": HOST_NAME,
+    }),
+    name="system_cpu_usage",
+    description="the system (user + kernel) CPU utilization in percentage, ranging [0, 100.0]",
+    unit="100%", # What should we put here? How do we distinguish [0, 1.0] versus [0, 100]?
+    value_type=int, # To save memory/network/storage, the dev decided to only expose int instead of float
+)
+
+meter.register_valueobserver(
+    callback=lambda observer: observer.observe(random.uniform(0, 100), {
+        "host": HOST_NAME,
+        "processId": PROCESS_ID,
+    }),
+    name="process_cpu_usage",
+    description="the process CPU utilization in percentage, ranging [0, 100.0]",
+    unit="100%",
+    value_type=float,
+)
+
+class OTelHttpRequestHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        start_time = time.time()
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(bytes("Hello, world!", "utf-8"))
+        elapsed_time = time.time() - start_time
+        # https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/semantic_conventions/http-metrics.md#labels
+        request_latency_recorder.record(elapsed_time * 1000, {
+            # Most of the dimension values are hard-coded just for sake of simplicity
+            "http.method": "GET",
+            "http.host": "host",
+            "http.scheme": "HTTP",
+            "http.status_code": 200, # It seems Python supports int
+            "http.flavor": "1.1",
+            "net.peer.ip": "127.0.0.1",
+            "net.peer.port": 51327,
+            "net.host.ip": "127.0.0.1",
+            "net.host.port": 80,
+        })
+
+    def do_POST(self):
+        content_length = int(self.headers["Content-Length"])
+        data = self.rfile.read(content_length)
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(bytes("Hello, world!", "utf-8"))
+        elapsed_time = time.time() - start_time
+        # https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/semantic_conventions/http-metrics.md#labels
+        request_latency_recorder.record(elapsed_time * 1000, {
+            # Most of the dimension values are hard-coded just for sake of simplicity
+            "http.method": "POST",
+            "http.host": "host",
+            "http.scheme": "HTTP",
+            "http.status_code": 200, # It seems Python supports int
+            "http.flavor": "1.1",
+            "net.peer.ip": "127.0.0.1",
+            "net.peer.port": 51327,
+            "net.host.ip": "127.0.0.1",
+            "net.host.port": 80,
+        })
